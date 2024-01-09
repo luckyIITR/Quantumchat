06ecb86559c7bdedbb7534a25c695bef06b4b893
Lucky Kushwaha <luckykushwaha56@gmail.com>
2024-01-09 11:42:05 +0530

    updated

--- Controllers/messageControllers.js
+++ Controllers/messageControllers.js
@@ -25,46 +25,35 @@ const allMessages = asyncHandler(async (req, res) => {
 const sendMessage = asyncHandler(async (req, res) => {
   req.user = await User.findById(req.user.id).select("-password");
   const { content, chatId } = req.body;
-  // console.log(req.body);
 
   if (!content || !chatId) {
     console.log("Invalid data passed into request");
     return res.sendStatus(400);
   }
 
-  async function createAndPopulateMessage(req, content, chatId) {
-    try {
-      const newMessage = {
-        sender: req.user._id,
-        content: content,
-        chat: chatId,
-      };
-  
-      const message = new Message(newMessage); // Create a new Message instance
-      await message.save(); // Save the message to the database
-  
-      const populatedMessage = await message.populate("sender", "name picture").exec(); // Populate the sender
-  
-      console.log(populatedMessage); // Output the populated message
+  var newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
 
-  // try {
-  //   // var message = Message(newMessage);
-  //   var p = 5;
-  //   // message = await message.populate("sender", "name pic").execPopulate();
-  //   // message = await message.populate("chat").execPopulate();
-  //   // message = await User.populate(message, {
-  //   //   path: "chat.users",
-  //   //   select: "name pic email",
-  //   // });
+  try {
+    var message = await Message.create(newMessage);
 
-  //   // await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+    message = await message.populate("sender", "name pic").execPopulate();
+    message = await message.populate("chat").execPopulate();
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
 
-  //   res.json(message);
-  // } catch (error) {
-  //   res.status(400);
-  //   throw new Error(error.message);
-  // }
-  res.status(200).json({});
+    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
 });
 
 export { allMessages, sendMessage };
